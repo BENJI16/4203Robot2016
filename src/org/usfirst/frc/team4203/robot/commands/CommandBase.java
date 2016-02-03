1612c958303319d1d743091e22c85d3483960ec4
BENJI16 <benjiblacklock@gmail.com>
2016-02-02 20:14:26 -0500

    Robotics Team 4203

---
+++ src/org/usfirst/frc/team4203/robot/commands/CommandBase.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team4203.robot.commands;
+
+// import all subsystems to be used by commands
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4203.robot.subsystems.DriveTrain;
+import org.usfirst.frc.team4203.robot.subsystems.Aimer;
+import org.usfirst.frc.team4203.robot.subsystems.Intake;
+import org.usfirst.frc.team4203.robot.subsystems.Pneumatics;
+import org.usfirst.frc.team4203.robot.subsystems.Shooter;
+import org.usfirst.frc.team4203.robot.OI;
+
+/**
+ *
+ */
+public abstract class CommandBase extends Command {
+
+	public static OI oi;
+	public static Aimer aimer = new Aimer();
+	public static Intake intake = new Intake();
+	public static Pneumatics pneumatics = new Pneumatics();
+	public static Shooter shooter = new Shooter();
+	public static DriveTrain driveTrain = new DriveTrain(); 
+
+    
+    public static void init() {
+    oi = new OI();
+    }
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
